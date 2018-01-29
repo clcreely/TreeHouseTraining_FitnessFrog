035f1e7a5a782f0b348e061218aee244791a5c1b
clare <ccreely@gmail.com>
2018-01-29 21:02:02 +0000

    pre refactoring

--- Program.cs
+++ Program.cs
@@ -11,26 +11,58 @@ namespace TreeHouseTraining_FitnessFrog
         static void Main(string[] args)
         {
 
-            int runningTotal = 0;
+            double runningTotal = 0;
             bool keepGoing = true;
 
             while (keepGoing)
             {
                 //Prompt the user to enter minutes exercised 
-                Console.Write("Enter how many minutes you exercised or type 'Quit' to exit: ");
+                Console.Write("Enter how many minutes you exercised or type 'quit' to exit: ");
                 string entry = Console.ReadLine();
 
-                if (entry == "Quit")
+                if (entry.ToLower() == "quit")
                 {
                     keepGoing = false;
                 }
 
                 else
                 {
-                    int minutes = int.Parse(entry);
-                    runningTotal = runningTotal + minutes;
-                    //Add minutes exercised to total
-                    //display total minutes exercised to the screen
+                    try
+                    {
+
+                        double minutes = double.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value, please try again.");
+                            continue;
+                        }
+
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing!");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay now you're showing off!");
+                        }
+
+                        runningTotal = runningTotal + minutes;
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not a valid input");
+                    }
+
                     Console.WriteLine("You've entered " + runningTotal + " minutes!");
                     //repeat until the user quits 
 
